8b3dbe891de4f64fcaafbc84d2f000a81dab5875
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2020-12-23 11:38:45 +0700

    new

---
+++ begin.R
@@ -0,0 +1,12 @@
+# this is a comment
+
+print("this is not a comment")
+
+# new tab ctrl+shift+n
+
+print("Hello")
+
+# clear console ctrl+l
+
+?plot
+help(mean)
---
+++ calculations.R
@@ -0,0 +1,25 @@
+3+6
+10-4
+3*8
+12/4
+5^2
+
+x <- 5
+y <- 7
+
+x + y
+
+class(x)
+typeof(x)
+
+myStr <- "Hello"
+
+class(myStr)
+typeof(myStr)
+
+myBool <- TRUE
+
+class(myBool)
+typeof(myBool)
+
+x + myBool
